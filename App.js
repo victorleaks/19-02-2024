251e27ea77fd972c208d5c438529d9c0d6591297
Victor <112430306+victorleaks@users.noreply.github.com>
2024-02-19 17:14:49 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import {exibirHorario} from './components/Horario';
+import {fazerLogin} from './components/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          id="lnk"
-        >
-          Learning React
-        </a>
-        <button
-        onClick={exibirHorario}
-        >Ver Horário</button>
-      </header>
-    </div>
-  );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="lnk"
+        >
+          Learning React
+        </a>
+        <br />
+        <input type="text" id="txtLogin" />
+        <br />
+        <input type="text" id="txtSenha" />
+        <br />
+        <button
+          onClick={fazerLogin}
+        >Fazer Login</button>
+        <br />
+        <button
+          onClick={exibirHorario}
+        >Ver Horário</button>
+      </header>
+    </div>
+  );
 }
-
-
 
 export default App;
--- Horario.jsx
+++ Horario.jsx
@@ -1,10 +1,10 @@
 export const exibirHorario = (e) => {
-    const dateObj = new Date();
-    const hora = dateObj.getHours();
-    const minuto = dateObj.getMinutes();
-    const segundo = dateObj.getSeconds();
-    let horarioFormatado = hora + ":" + minuto + ":" + segundo;
-    console.clear();
-    console.log(horarioFormatado);
-    document.getElementById("lnk").innerText = horarioFormatado;
-}
+        const dateObj = new Date();
+        const hora = dateObj.getHours();
+        const minuto = dateObj.getMinutes();
+        const segundo = dateObj.getSeconds();
+        let horarioFormatado = hora + "h:" + minuto + "m:" + segundo + "s";
+        console.clear();
+        console.log(horarioFormatado);
+        document.getElementById("lnk").innerText = horarioFormatado;
+    }
